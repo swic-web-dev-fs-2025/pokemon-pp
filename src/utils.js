12e11ada6376fe34cb223be8ac8768f0bf01e94a
Elijah Peiffer <elijah.peiffer@student.swic.edu>
2025-10-20 19:26:47 -0500

    feat: implement calculateAverageHP function

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,20 @@
-
 export const filterByType = (pokemonList, type) => {
-  return pokemonList.filter(pokemon => pokemon.type === type);
+  return pokemonList.filter((pokemon) => pokemon.type === type);
 };
 
 export const getPokemonNames = (pokemonList) => {
-  return pokemonList.map(pokemon => pokemon.name);
+  return pokemonList.map((pokemon) => pokemon.name);
 };
 
 export const getStrongestPokemon = (pokemonList) => {
   let strongest = pokemonList[0];
-  
+
   for (let i = 1; i < pokemonList.length; i++) {
     if (pokemonList[i].attack > strongest.attack) {
       strongest = pokemonList[i];
     }
   }
-  
+
   return strongest;
 };
 
@@ -24,4 +23,10 @@ export const sortByName = (pokemonList) => {
   // Hint: Use array sort method
   // Remember: Don't mutate the original array!
   return pokemonList;
+};
+
+export const calculateAverageHP = (pokemonList) => {
+  if (pokemonList.length === 0) return 0;
+  const totalHP = pokemonList.reduce((sum, pokemon) => sum + pokemon.hp, 0);
+  return totalHP / pokemonList.length;
 };
